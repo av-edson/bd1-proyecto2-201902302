7fe29b5c57227c0979dcca8cb108ea6abc7637fa
Edson Saul Avila Ortiz <3685020900101@ingenieria.usac.edu.gt>
2021-10-31 15:52:04 -0600

    solo faltan 2 consultas

--- Consultas.sql
+++ Consultas.sql
@@ -27,6 +27,34 @@ select votPart.elec,votPart.año,pais.nombre,votPart.parti,((votPart.votos_total
     select max(votPart.votos_totales) from votPart where votPart.nombre=pais.nombre
     ) and 
 total.nombre=votPart.nombre;
+-- ================================== CONSULTA 2  ===========================================
+with votos_mujeres as (
+    select pa.nombre,dep.nombre depto,pa.id_pais,dep.id_depto,
+    sum(edu.alfabetos+edu.analfabetos+edu.primaria+edu.nivel_medio+edu.universidad)votos
+    from pais pa inner join region re inner join departamento dep inner join 
+    municipio mun inner join eleccion ele inner join partido_eleccion pae 
+    inner join partido par inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion and vot.id_sexo=(select id_sexo from sexo where
+    sexo='mujeres') group by pa.nombre,dep.nombre
+), totales as (
+    select pa.nombre,dep.nombre depto,pa.id_pais,dep.id_depto,
+    sum(edu.alfabetos+edu.analfabetos+edu.primaria+edu.nivel_medio+edu.universidad)votos
+    from pais pa inner join region re inner join departamento dep inner join 
+    municipio mun inner join eleccion ele inner join partido_eleccion pae 
+    inner join partido par inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion and vot.id_sexo=(select id_sexo from sexo where
+    sexo='mujeres') group by pa.nombre
+) select totales.nombre pais,votos_mujeres.depto depto,votos_mujeres.votos votos_mujeres,
+totales.votos votos_pais,
+(votos_mujeres.votos/totales.votos*100)porcentaje_mujeres
+from totales inner join votos_mujeres on votos_mujeres.id_pais=totales.id_pais 
+group by pais,depto;
 -- ================================== CONSULTA 4  ===========================================
 with votRaz as (
     select pa.nombre,re.nombre reg,(select descripcion from raza where id_raza=vot.id_raza) raza,
@@ -103,6 +131,82 @@ with departa as (
     (select (votos_uni/departa.votos*100) from vots where 
     vots.id_pais=departa.id_pais and vots.id_depto=departa.id_depto and vots.id_sexo=(select 
 id_sexo from sexo where sexo='hombres'));
+-- ================================== CONSULTA 7  ==========================================
+with dat as (
+    select pa.nombre pais,re.nombre region,pa.id_pais,re.id_region,
+    sum(edu.alfabetos+edu.analfabetos+edu.primaria+edu.nivel_medio+edu.universidad)votos
+    from pais pa
+    inner join region re inner join departamento dep inner join municipio mun
+    inner join eleccion ele inner join partido_eleccion pae inner join partido par
+    inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion  group by pa.id_pais,re.id_region
+) select dat.pais,dat.region,dat.votos,
+(select count(id_depto) from departamento where id_region=dat.id_region)deptos,
+(dat.votos/(select count(id_depto) from departamento where id_region=dat.id_region))prom
+from dat;
+-- ================================== CONSULTA 8  ==========================================
+select pa.nombre,
+    sum(edu.primaria)prim,sum(edu.nivel_medio)med,sum(edu.universidad)uni
+    from pais pa
+    inner join region re inner join departamento dep inner join municipio mun
+    inner join eleccion ele inner join partido_eleccion pae inner join partido par
+    inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+and edu.id_educacion=vot.id_educacion  group by pa.nombre;
+-- ================================== CONSULTA 9  ==========================================
+with votRaz as (
+    select pa.nombre,(select descripcion from raza where id_raza=vot.id_raza)raza,pa.id_pais,
+    sum(edu.alfabetos+edu.analfabetos+edu.primaria+edu.nivel_medio+edu.universidad)votos
+    from pais pa
+    inner join region re inner join departamento dep inner join municipio mun
+    inner join eleccion ele inner join partido_eleccion pae inner join partido par
+    inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion  group by pa.nombre,vot.id_raza
+), votPai as (
+    select pa.nombre,pa.id_pais,
+    sum(edu.alfabetos+edu.analfabetos+edu.primaria+edu.nivel_medio+edu.universidad)votos_totales
+    from pais pa
+    inner join region re inner join departamento dep inner join municipio mun
+    inner join eleccion ele inner join partido_eleccion pae inner join partido par
+    inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion  group by pa.nombre
+) select votPai.nombre,votRaz.raza,
+(votRaz.votos/votPai.votos_totales*100)porcentaje from votRaz inner join 
+votPai on votPai.id_pais=votRaz.id_pais group by votPai.nombre,votRaz.raza;
+-- ================================== CONSULTA 10 ==========================================
+create or replace view votosPaisPartido as (
+    select pa.nombre pais,par.nombre partido,pa.id_pais,par.id_partido,
+    sum(edu.alfabetos+edu.analfabetos+edu.primaria+edu.nivel_medio+edu.universidad)votos
+    from pais pa
+    inner join region re inner join departamento dep inner join municipio mun
+    inner join eleccion ele inner join partido_eleccion pae inner join partido par
+    inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion  group by pa.nombre,par.nombre
+);
+with valores as (
+    select pa.nombre,pa.id_pais,votosPaisPartido.partido,votosPaisPartido.votos from pais pa 
+    inner join votosPaisPartido on pa.id_pais=votosPaisPartido.id_pais and (votosPaisPartido.votos=
+    (select max(dd.votos) from votosPaisPartido dd where dd.id_pais=pa.id_pais) or votosPaisPartido.votos
+    =(select min(dd.votos) from votosPaisPartido dd where dd.id_pais=pa.id_pais))
+) 
+select pa.nombre,((select max(votos) from valores where id_pais=pa.id_pais) -
+    (select min(votos) from valores where id_pais=pa.id_pais))diferencia
+    from valores inner join pais pa on
+pa.id_pais=valores.id_pais group by pa.nombre order by diferencia limit 1;
 -- ================================== CONSULTA 11 ===========================================
 with muj as (
     select pa.nombre,
@@ -129,3 +233,25 @@ pai as (
     and edu.id_educacion=vot.id_educacion  group by pa.nombre
 ) select muj.nombre,muj.votos,(muj.votos/pai.votos*100) porcentaje
 from pai, muj where pai.nombre=muj.nombre;
+-- ================================== CONSULTA 12 ===========================================
+with analfabetos as (    
+    select pa.nombre,pa.id_pais,sum(edu.analfabetos)votos
+    from pais pa inner join region re inner join departamento dep inner join 
+    municipio mun inner join eleccion ele inner join partido_eleccion pae 
+    inner join partido par inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion  group by pa.nombre
+), totales as (
+    select sum(edu.analfabetos)votos
+    from pais pa inner join region re inner join departamento dep inner join 
+    municipio mun inner join eleccion ele inner join partido_eleccion pae 
+    inner join partido par inner join voto vot inner join educacion edu
+    on pa.id_pais=re.id_pais and re.id_region=dep.id_region and dep.id_depto=mun.id_depto
+    and ele.id_municipio=mun.id_municipio and pae.id_eleccion=ele.id_eleccion 
+    and par.id_partido=pae.id_partido and vot.id_partido_eleccion=pae.id_partido_eleccion
+    and edu.id_educacion=vot.id_educacion
+)select analfabetos.nombre,(analfabetos.votos/totales.votos*100)porcentaje
+from analfabetos,totales order by porcentaje desc limit 1;
+-- ================================== CONSULTA 13 ===========================================
